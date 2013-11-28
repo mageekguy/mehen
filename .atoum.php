fbfec6528a020a7ad27e7a9e6a5608bce609f906
julien.bianchi <julien.bianchi@pmsipilot.com>
2013-11-28 11:33:05 +0100

    Use atoum from vendor

---
+++ .atoum.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace mehen;
+
+use mageekguy\atoum;
+
+$runner->addTestsFromDirectory(__DIR__ . '/tests');
--- tests/units/runner.php
+++ tests/units/runner.php
@@ -4,11 +4,9 @@ namespace mehen;
 
 use atoum;
 
-require_once __DIR__ . '/../../autoloader.php';
-
 if (defined('atoum\scripts\runner') === false)
 {
 	define('atoum\scripts\runner', __FILE__);
 }
 
-require_once __DIR__ . '/atoum/scripts/runner.php';
+require_once __DIR__ . '/../../vendor/atoum/atoum/scripts/runner.php';
