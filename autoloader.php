bf68d81ac2359e3af7b922236ea4c32e3a129898
jubianchi <contact@jubianchi.fr>
2014-02-15 13:05:43 +0100

    Add autoloader from mageekguy/autoloader

---
+++ autoloader.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace mehen;
+
+$autoloader = function($directories, $fileFormat = null, $namespaceAliases = null, $classAliases = null) {
+	$fileFormat = $fileFormat ?: '%s.php';
+	$namespaceAliases = $namespaceAliases ?: array();
+	$classAliases = $classAliases ?: array();
+
+	spl_autoload_register(function($class) use ($fileFormat, $directories, $namespaceAliases, $classAliases) {
+			$realClass = (isset($classAliases[$class]) === false ? $class : $classAliases[$class]);
+
+			foreach ($namespaceAliases as $alias => $namespace)
+			{
+				$aliasLength = strlen($alias);
+
+				if ($realClass !== $alias && strncmp($alias, $realClass, $aliasLength) === 0)
+				{
+					$realClass = $namespace . substr($class, $aliasLength);
+
+					break;
+				}
+			}
+
+			if ($realClass === $class || class_exists($realClass, false) === false) foreach ($directories as $namespace => $directory)
+			{
+				$namespaceLength = strlen($namespace);
+
+				if ($realClass !== $namespace && strncmp($namespace, $realClass, $namespaceLength) === 0)
+				{
+					$path = $directory . sprintf($fileFormat, str_replace('\\', DIRECTORY_SEPARATOR, substr($realClass, $namespaceLength)));
+
+					if (is_file($path) === true)
+					{
+						require $path;
+					}
+
+					break;
+				}
+			}
+
+			if ($realClass !== $class && class_exists($realClass, false) === true)
+			{
+				class_alias($realClass, $class);
+			}
+		}
+	);
+};
+
+$autoloader(
+	array(
+		__NAMESPACE__ => __DIR__ . '/classes'
+	)
+);
